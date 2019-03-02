d432de95003d68fdfd2d51bfd7e956ab9ee98f62
IVANDI <41560792+Ivandidjoh@users.noreply.github.com>
2019-03-03 02:38:57 +0700

    update data.json & modify main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,5 @@
 $(document).ready(function () {
 
-    $("#name").on('keyup', function () {
-        $("#name").removeClass('is-invalid');
-    });
-
-    $("#npwp").on('keyup', function () {
-        $("#npwp").removeClass('is-invalid');
-    });
-
-    $("#monthlyIncome").on('keyup', function () {
-        $("#monthlyIncome").removeClass('is-invalid');
-        var temp2 = $(this).val().replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        $(this).val(temp2);
-    });
-
     // count percentage
     function getPercentage(value, percentValue) {
         return (value * percentValue) / 100;
@@ -53,11 +39,26 @@ $(document).ready(function () {
         }
     });
 
+
+    $("#monthlyIncome").on('keyup', function () {
+        $("#monthlyIncome").removeClass('is-invalid');
+        var temp2 = $(this).val().replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        $(this).val(temp2);
+    });
+
+    $("#name").on('keyup', function () {
+        $("#name").removeClass('is-invalid');
+    });
+
+    $("#npwp").on('keyup', function () {
+        $("#npwp").removeClass('is-invalid');
+    });
+
     $.getJSON("data.json", function (data) {
         $("#calculate").click(function () {
             if ($("#name").val() == '') {
                 $("#name").addClass('is-invalid');
-                return
+                // return
             } else if ($("#npwp").val() == '') {
                 $("#npwp").addClass('is-invalid');
                 return
@@ -81,6 +82,9 @@ $(document).ready(function () {
             var status = $("#status").val();
             var dependent = $("#dependent").val();
 
+            var jkk = $("#jkk").val();
+            var jkm = $("#jkm").val();
+
             // convert to integer
             var taxRate1Num = parseInt(taxRate1);
             var taxRate2Num = parseInt(taxRate2);
@@ -93,6 +97,30 @@ $(document).ready(function () {
 
             var monthlyIncomeNum = parseInt(monthlyIncome);
             var annualIncomeNum = parseInt(annualIncome);
+
+            var jkkNum = parseInt(jkk);
+            var jkmNum = parseInt(jkm);
+            var getJkk = getPercentage(monthlyIncomeNum, 0.50);
+            var getJkm = getPercentage(monthlyIncomeNum, 0.30);
+            var getJkkNum = parseInt(getJkk);
+            var getJkmNum = parseInt(getJkm);
+
+            var iuranPensiun = data.iuran_pensiun;
+            var iuranJaminanHariTua = data.iuran_jaminan_hari_tua;
+
+            var iuranPensiunNum = parseInt(iuranPensiun);
+            var iuranJaminanHariTuaNum = parseInt(iuranJaminanHariTua);
+
+            var grossMonthly = monthlyIncomeNum + getJkkNum + getJkmNum;
+            var grossMonthlyNum = parseInt(grossMonthly);
+            
+
+            var biayaJabatan = getPercentage(grossMonthlyNum, data.biaya_jabatan);
+            var biayaJabatanNum = parseInt(biayaJabatan);
+
+            var reduction = (biayaJabatanNum + iuranPensiunNum + iuranJaminanHariTuaNum);
+            var netAnnualIncome = (grossMonthlyNum - reduction)*12;
+            var netAnnualIncomeNum = parseInt(netAnnualIncome);
 
             // get result without allowence
             function getResult(value) {
@@ -177,48 +205,58 @@ $(document).ready(function () {
 
             } else {
 
-                if (annualIncomeNum >= 0 && annualIncomeNum <= salary1Num) {
-                    var stepNoDependent1 = getPercentage(annualIncomeNum, taxRate1Num);
+                if (netAnnualIncomeNum >= 0 && netAnnualIncomeNum <= salary1Num) {
+                    var stepNoDependent1 = getPercentage(netAnnualIncomeNum, taxRate1Num);
                     var finalResult = stepNoDependent1;
 
-                } else if (annualIncomeNum > salary1Num && annualIncomeNum <= salary2Num) {
+                } else if (netAnnualIncomeNum > salary1Num && netAnnualIncomeNum <= salary2Num) {
                     var stepNoDependent1 = getPercentage(salary1Num, taxRate1Num);
 
-                    if ((annualIncomeNum - salary1Num) > 0) {
-                        var stepNoDependent2 = annualIncomeNum - salary1Num;
+                    if ((netAnnualIncomeNum - salary1Num) > 0) {
+                        var stepNoDependent2 = netAnnualIncomeNum - salary1Num;
                         var stepNoDependent3 = getPercentage(stepNoDependent2, taxRate2Num);
                         var finalResult = stepNoDependent1 + stepNoDependent3;
                     }
 
-                } else if (annualIncomeNum > salary2Num && annualIncomeNum <= salary3Num) {
+                } else if (netAnnualIncomeNum > salary2Num && netAnnualIncomeNum <= salary3Num) {
                     var stepNoDependent1 = getPercentage(salary1Num, taxRate1Num);
                     var stepNoDependent2 = salary2Num - salary1Num;
                     var stepNoDependent3 = getPercentage(stepNoDependent2, taxRate2Num);
 
-                    if ((annualIncomeNum - (salary1Num + stepNoDependent2)) > 0) {
-                        var stepNoDependent4 = annualIncomeNum - (salary1Num + stepNoDependent2);
+                    if ((netAnnualIncomeNum - (salary1Num + stepNoDependent2)) > 0) {
+                        var stepNoDependent4 = netAnnualIncomeNum - (salary1Num + stepNoDependent2);
                         var stepNoDependent5 = getPercentage(stepNoDependent4, taxRate3Num);
                         var finalResult = stepNoDependent1 + stepNoDependent3 + stepNoDependent5;
                     }
 
-                } else if (annualIncomeNum > salary3Num) {
+                } else if (netAnnualIncomeNum > salary3Num) {
                     var stepNoDependent1 = getPercentage(salary1Num, taxRate1Num);
                     var stepNoDependent2 = salary2Num - salary1Num;
                     var stepNoDependent3 = getPercentage(stepNoDependent2, taxRate2Num);
                     var stepNoDependent4 = salary3Num - stepNoDependent2;
                     var stepNoDependent5 = getPercentage(stepNoDependent4, taxRate3Num);
 
-                    if ((annualIncomeNum - (salary1Num + stepNoDependent2 + stepNoDependent4)) > 0) {
-                        var stepNoDependent6 = annualIncomeNum - (salary1Num + stepNoDependent2 + stepNoDependent4);
+                    if ((netAnnualIncomeNum - (salary1Num + stepNoDependent2 + stepNoDependent4)) > 0) {
+                        var stepNoDependent6 = netAnnualIncomeNum - (salary1Num + stepNoDependent2 + stepNoDependent4);
                         var stepNoDependent7 = getPercentage(stepNoDependent6, taxRate4Num);
                         var finalResult = stepNoDependent1 + stepNoDependent3 + stepNoDependent5 + stepNoDependent7;
                     }
 
                 }
 
-                $("#grossMonthly").val(monthlyIncomeNum.toLocaleString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
-                $("#grossAnnual, #annualTaxable").val(annualIncomeNum.toLocaleString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+                $("#grossMonthly").val(grossMonthlyNum.toLocaleString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+                $("#grossAnnual").val((grossMonthlyNum*12).toLocaleString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+                $("#netMonthly").val((grossMonthlyNum - reduction).toLocaleString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+                $("#netAnnual").val(((grossMonthlyNum - reduction)*12).toLocaleString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
                 $("#annualTax").val(finalResult.toLocaleString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+                $("#jkk").val(getJkkNum.toLocaleString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+                $("#jkm").val(getJkmNum.toLocaleString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+                $("#biayaJabatan").val(biayaJabatanNum.toLocaleString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+
+
+                
+                console.log(iuranPensiunNum + iuranJaminanHariTuaNum + biayaJabatanNum)
+                
             }
         });
 
