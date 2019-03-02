1305aa2e0f8503575913f596b35224fe381d3424
IVANDI <41560792+Ivandidjoh@users.noreply.github.com>
2019-03-02 20:14:16 +0700

    edit script main.js by adding allowance checkbox

--- js/main.js
+++ js/main.js
@@ -14,6 +14,41 @@ $(document).ready(function () {
         $(this).val(temp2);
     });
 
+
+    // allowence checkbox
+    $("#allowence").click(function () {
+        $("#allowence").toggleClass("check");
+        if ($("#allowence").hasClass("check")) {
+            $("#status").removeAttr("disabled");
+            $("#status").val("single").change();
+            $("#dependent").val("tk0").change();
+            $("#status option[value='unset']").remove();
+        } else {
+            $("#status").attr("disabled", "disabled");
+            $("#dependent").attr("disabled", "disabled");
+        }
+    });
+
+    $("#dependent").click(function () {
+        if ($("#dependent").val() == "unset") {
+            $("#dependent").removeClass('is-invalid');
+        }
+    });
+
+    $("#status").change(function () {
+        if ($("#status").val() == "married") {
+            $("#dependent").removeAttr("disabled");
+            $("#dependent option[value='tk0']").remove();
+        } else if ($("#status").val() == "single") {
+            $("#dependent").append(new Option("TK0 - Single with no dependent", "tk0"));
+            $("#dependent").attr("disabled", "disabled");
+            $("#dependent").val("tk0").change();
+        } else {
+            $("#dependent").attr("disabled", "disabled");
+            $("#dependent").val("unset").change();
+        }
+    });
+
     // count percentage
     function getPercentage(value, percentValue) {
         return (value * percentValue) / 100;
